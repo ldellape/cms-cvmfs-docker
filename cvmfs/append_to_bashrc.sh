a2662fb7447a8b9e25f5fdea79344bcb9a9ce220
Alexx Perloff <Alexx.Perloff@Colorado.edu>
2018-10-24 12:45:36 -0500

    Reconfigure the gitignore file and add new line to bashrc. The new line allows XrootD access to FNAL EOS.

--- cvmfs/append_to_bashrc.sh
+++ cvmfs/append_to_bashrc.sh
@@ -11,6 +11,9 @@ else
     ./run.sh
 fi
 
-# source some CMS/VOMS specific setup scripts
+# Source some CMS/VOMS specific setup scripts
 source /cvmfs/cms.cern.ch/cmsset_default.sh
 source /cvmfs/oasis.opensciencegrid.org/mis/osg-wn-client/current/el6-x86_64/setup.sh
+
+# Needed to access FNAL EOS
+export XrdSecGSISRVNAMES="cmseos.fnal.gov"
