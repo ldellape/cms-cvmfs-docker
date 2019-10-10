980ff14d0e5b8800d5bace02d7ec1ddfaf72c5f9
Alexx Perloff <Alexx.Perloff@Colorado.edu>
2019-10-10 18:31:08 -0500

    Add the option to only mount specific CVMFS folders. Specify them using the '-e CVMFS_MOUNTS="<space separated list of mount names>"' option for 'docker run'

--- cvmfs/run.sh
+++ cvmfs/run.sh
@@ -1,7 +1,17 @@
 #!/bin/bash
 mknod /dev/fuse c 10 229
 chmod a+w /dev/fuse
-mount -a
+if [ -z "$CVMFS_MOUNTS" ]; then
+    echo -ne "Mounting all filesystems in /etc/fstab ... "
+    mount -a -F > /dev/null 2>&1
+    echo -e "DONE"
+else
+    for MOUNT in ${CVMFS_MOUNTS[@]}; do
+	echo -ne "Mounting the filesystem \"${MOUNT}\" ... "
+	mount -F ${MOUNT} > /dev/null 2>&1
+	echo -e "DONE"
+    done
+fi
 
 # Set the default shell as bash for docker user.
 chsh -s /bin/bash root
